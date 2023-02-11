80ac3287faae640f368f77d66cecd4280257b906
mad-of-gravity <mad.of.gravity@gmail.com>
2023-02-12 00:49:12 +0200

    Header has been created

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,6 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import createEmotionServer from '@emotion/server/create-instance';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
+import Header from '../src/components/header/Header';
 
 export default class MyDocument extends Document {
   render() {
