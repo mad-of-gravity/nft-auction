ab756229d7892583f81f824b55d65064968fcc9e
mad-of-gravity <mad.of.gravity@gmail.com>
2023-02-12 01:43:08 +0200

    Badge component has been created

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Example from "../src/components/example/Example";
 import Header from "../src/components/header/Header";
+import Avatar from "../src/components/avatar/Avatar";
 
 export default function Index() {
-  return <Header />;
+  return <Avatar url="images/avatar.png" size={160} verified={true}/>;
 }
