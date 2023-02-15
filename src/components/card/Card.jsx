945b009c56d5b8f006c226c518297bba8a4f3ebe
mad-of-gravity <mad.of.gravity@gmail.com>
2023-02-15 10:46:28 +0200

    fix validation bugs

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -23,7 +23,7 @@ export default function Card({
   currency = "",
   ...props
 }) {
-  const millifiedLikes = millify(likes, { lowercase: true });
+  const millifiedLikes = millify(likes);
 
   return (
     <CardMUI className={classNames(styles.card)}>
