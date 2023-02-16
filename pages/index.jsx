6539dc5971d29b17f20123ae119897de70680a55
mad-of-gravity <mad.of.gravity@gmail.com>
2023-02-16 20:20:35 +0200

    Trnding components has been created

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import Header from "../src/components/header/Header";
 import Avatar from "../src/components/avatar/Avatar";
 import User from "../src/components/user/User";
 import Card from "../src/components/card/Card";
+import Trending from "../src/components/trending/Trending";
 
 const user = {
   avatar: {
@@ -11,6 +12,59 @@ const user = {
   verified: true,
 }
 
+const cardData = {
+  "cards":[
+     {
+        "name":"Ivy",
+        "user":{
+           "avatar":{
+              "url":"images/avatar.png"
+           },
+           "verified":true,
+        },
+        "mediaUrl":"images/nft.jpg",
+        "price":1,
+        "currency":"ETH"
+     },
+     {
+        "name":"Judie",
+        "user":{
+           "avatar":{
+              "url":"images/avatar.png"
+           },
+           "verified":true
+        },
+        "mediaUrl":"images/nft.jpg",
+        "price":2.3,
+        "currency":"ETH"
+     },
+     {
+        "name":"Juniper",
+        "user":{
+           "avatar":{
+              "url":"images/avatar.png"
+           },
+           "verified":true
+        },
+        "mediaUrl":"images/nft.jpg",
+        "price":5,
+        "currency":"ETH"
+     },
+     {
+        "name":"Maple",
+        "user":{
+           "avatar":{
+              "url":"images/avatar.png"
+           },
+           "verified":true
+        },
+        "mediaUrl":"images/nft.jpg",
+        "price":10,
+        "currency":"ETH"
+     }
+  ]
+} 
+
 export default function Index() {
-  return <Card name="Clock" user={user} size={32} mediaUrl="images/nft.jpg" price="~12.2" currency="ETH" likes="1100"/>;
+  return <Trending cards={cardData.cards} />;
 }
---
+++ src/components/trending/Trending.jsx
@@ -0,0 +1,49 @@
+import {
+  Box,
+  Grid,
+  Container,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Stack,
+} from "@mui/material";
+import styles from "./Trending.module.scss";
+import classNames from "classnames";
+import Card from "../card/Card";
+
+export default function Trending({ cards = [] }) {
+  return (
+    <Container className={classNames(styles.trendingContainer)}>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        className={classNames(styles.headerBox)}
+      >
+        <h1 className={classNames(styles.heading)}>Trending</h1>
+        <FormControl className={classNames(styles.formControl)}>
+          <InputLabel id="trending-select">This week</InputLabel>
+          <Select
+            labelId="trending-select"
+            id="select"
+            className={classNames(styles.select)}
+          >
+            <MenuItem value={1}>Sunday</MenuItem>
+            <MenuItem value={2}>Monday</MenuItem>
+            <MenuItem value={3}>Tuesday</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+
+      <Grid container rowSpacing={1} columnSpacing={1} justifyContent="center">
+        {cards &&
+          cards.map((card, index) => (
+            <Grid item key={index} sm={12} md={3} lg={3} xl={3}>
+              <Card size={35} {...card} className={classNames(styles.card)} />
+            </Grid>
+          ))}
+      </Grid>
+    </Container>
+  );
+}
