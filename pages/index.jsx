cd5948955677d564e4488ae5af10a46e2d9d7629
mad-of-gravity <mad.of.gravity@gmail.com>
2023-02-17 02:43:29 +0200

    Live state has been add to component

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,66 +5,77 @@ import User from "../src/components/user/User";
 import Card from "../src/components/card/Card";
 import Trending from "../src/components/trending/Trending";
 
-const user = {
-  avatar: {
-    url: "images/avatar.png",
-  },
-  verified: true,
-}
 
 const cardData = {
-  "cards":[
-     {
-        "name":"Ivy",
-        "user":{
-           "avatar":{
-              "url":"images/avatar.png"
-           },
-           "verified":true,
+  cards: [
+    {
+      name: "Ivy",
+      user: {
+        avatar: {
+          url: "images/avatar.png",
         },
-        "mediaUrl":"images/nft.jpg",
-        "price":1,
-        "currency":"ETH"
-     },
-     {
-        "name":"Judie",
-        "user":{
-           "avatar":{
-              "url":"images/avatar.png"
-           },
-           "verified":true
+        verified: true,
+      },
+      mediaUrl: "images/nft.jpg",
+      price: 1,
+      currency: "ETH",
+    },
+    {
+      name: "Judie",
+      user: {
+        avatar: {
+          url: "images/avatar.png",
         },
-        "mediaUrl":"images/nft.jpg",
-        "price":2.3,
-        "currency":"ETH"
-     },
-     {
-        "name":"Juniper",
-        "user":{
-           "avatar":{
-              "url":"images/avatar.png"
-           },
-           "verified":true
+        verified: true,
+      },
+      mediaUrl: "images/nft.jpg",
+      price: 2.3,
+      currency: "ETH",
+    },
+    {
+      name: "Juniper",
+      user: {
+        avatar: {
+          url: "images/avatar.png",
         },
-        "mediaUrl":"images/nft.jpg",
-        "price":5,
-        "currency":"ETH"
-     },
-     {
-        "name":"Maple",
-        "user":{
-           "avatar":{
-              "url":"images/avatar.png"
-           },
-           "verified":true
+        verified: true,
+      },
+      mediaUrl: "images/nft.jpg",
+      price: 5,
+      currency: "ETH",
+    },
+    {
+      name: "Maple",
+      user: {
+        avatar: {
+          url: "images/avatar.png",
         },
-        "mediaUrl":"images/nft.jpg",
-        "price":10,
-        "currency":"ETH"
-     }
-  ]
-} 
+        verified: true,
+      },
+      mediaUrl: "images/nft.jpg",
+      price: 10,
+      currency: "ETH",
+    },
+  ],
+};
+
+const user = {
+   avatarUrl: "images/avatar.png",
+   verified: true,
+}
+
 
 export default function Index() {
-  return <Trending cards={cardData.cards} />;
+  return (
+    <Card
+      name="Suspect of change"
+      likes={1200}
+      price="2.3"
+      currency="ETH"
+      mediaUrl="images/nft.jpg"
+      user={user}
+      size={35}
+      timeLeft={13000}
+    />
+  );
 }
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,54 +1,97 @@
+import { useState, useEffect } from "react";
 import Avatar from "../avatar/Avatar";
-import {
-  Box,
-  Card as CardMUI,
-  CardContent,
-  CardMedia,
-  Chip,
-} from "@mui/material";
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import { Card as CardMUI, cardClasses, CardContent, Chip } from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import CircleIcon from "@mui/icons-material/Circle";
 import styles from "./Card.module.scss";
 import classNames from "classnames";
 import millify from "millify";
+import Countdown from "react-countdown";
 
 export default function Card({
   name = "",
   likes = 0,
   mediaUrl = "",
   user = {
-    avatar: { url: "" },
+    avatarUrl: "",
     verified: false,
   },
   price = "",
   currency = "",
+  timeLeft,
   ...props
 }) {
-  const millifiedLikes = millify(likes, {lowercase: true});
+  const [live, setLive] = useState(false);
 
+  useEffect(() => {
+    const timeleftExists = timeLeft ? true : false;
+    setLive(timeleftExists);
+  }, []);
+
+  //Countdown render function
+  const renderer = ({ hours, minutes, seconds, completed }) => {
+    if (completed) {
+      // Render a completed state
+      setLive(false);
+      return null;
+    } else {
+      // Render a countdown
+      const currentHour = hours >= 10 ? hours : `0${hours}`;
+      const currentMinutes = minutes >= 10 ? minutes : `0${minutes}`;
+      const currentSeconds = seconds >= 10 ? seconds : `0${seconds}`;
+
+      return (
+        <span>
+          {currentHour}:{currentMinutes}:{currentSeconds}
+        </span>
+      );
+    }
+  };
+
+  const millifiedLikes = millify(likes, { lowercase: true });
   return (
-    <CardMUI className={classNames(styles.card)}>
-        <CardContent>
-          <Avatar
-            url={user.avatar.url}
-            verified={user.verified}
-            size={props.size}
+    <CardMUI className={classNames(styles.card)} sx={{
+      backgroundColor: (live) ? "rgba(36, 242, 94, .1)" : null,
+      
+    }}>
+      <CardContent>
+        <Avatar
+          url={user.avatarUrl}
+          verified={user.verified}
+          size={props.size}
+        />
+        <div className={classNames(styles.nftContainer)}>
+          <img
+            className={classNames(styles.media)}
+            src={mediaUrl}
+            alt="NFT image"
           />
-          <div className={classNames(styles.nftContainer)}>
-            <img className={classNames(styles.media)} src={mediaUrl} alt="NFT image" />
+          {live ? (
+            <>
+              <div className={classNames(styles.badge)}>
+                <CircleIcon className={classNames(styles.circle)}/> LIVE
+              </div>
+              <div className={styles.counter}>
+                <Countdown date={Date.now() + timeLeft} renderer={renderer} />
+              </div>
+            </>
+          ) : null}
+        </div>
+        <div className={classNames(styles.nftFooter)}>
+          <div className={classNames(styles.nftInfo)}>
+            <h2 className={classNames(styles.title)}>{name}</h2>
+            <h3 className={classNames(styles.price)}>
+              {price} {currency}
+            </h3>
           </div>
-          <div className={classNames(styles.nftFooter)}>
-            <div className={classNames(styles.nftInfo)}>
-              <h2 className={classNames(styles.title)}>{name}</h2>
-              <h3 className={classNames(styles.price)}>{price} {currency}</h3>
-            </div>
-            <Chip
-                icon={<FavoriteIcon className={classNames(styles.favoriteIcon)}/>}
-                label={millifiedLikes}
-                variant="outlined"
-                className={classNames(styles.likes)}
-              />
-          </div>
-        </CardContent>
-      </CardMUI>
+          <Chip
+            icon={<FavoriteIcon className={classNames(styles.favoriteIcon)} />}
+            label={millifiedLikes}
+            variant="outlined"
+            className={classNames(styles.likes)}
+          />
+        </div>
+      </CardContent>
+    </CardMUI>
   );
 }
