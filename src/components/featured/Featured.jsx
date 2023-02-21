529d7ba6c09ec3d0ba4bc4ca965fa1b850df1f6a
mad-of-gravity <mad.of.gravity@gmail.com>
2023-02-21 20:21:48 +0200

    fix validation bugs

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,36 +1,43 @@
-
 import { useRouter } from "next/dist/client/router";
 import styles from "./Featured.module.scss";
 import classNames from "classnames";
-import { Container, ImageList, ImageListItem } from "@mui/material";
+import Container from "@mui/material/Container";
+import { ImageList, ImageListItem } from "@mui/material";
 
-export default function Featured({items= []}) {
+export default function Featured({ items = [] }) {
   const router = useRouter();
 
   const handleImageClick = (i) => {
     router.push(items[i].href);
-  }
+  };
 
   return (
-    <Container maxWidth="xl">
-      <ImageList 
-        className={styles.imageList} 
-        variant="quilted"
-        cols={6}
-        gap={15}
-      >
-        { items && items.map((item, index) => 
-            <ImageListItem key={index} cols={item.cols || 1} rows={item.rows || 1}>
-              <img
-                src={item.image}
-                alt={item.title}
-                loading="lazy"
-                className={classNames(styles.image)}
-                onClick={handleImageClick(index)}
-              />
-          </ImageListItem>)
-        }
-      </ImageList>
-    </Container>
+    <>
+      <Container maxWidth="xl">
+        <ImageList
+          className={styles.imageList}
+          variant="quilted"
+          cols={6}
+          gap={15}
+        >
+          {items &&
+            items.map((item, index) => (
+              <ImageListItem
+                key={index}
+                cols={item.cols || 1}
+                rows={item.rows || 1}
+              >
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  loading="lazy"
+                  className={classNames(styles.image)}
+                  onClick={handleImageClick(index)}
+                />
+              </ImageListItem>
+            ))}
+        </ImageList>
+      </Container>
+    </>
   );
 }
